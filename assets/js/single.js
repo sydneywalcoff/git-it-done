a8f3cc8d16db9fa5e7ce6995a495e8f4d94ad606
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-03-03 11:44:23 -0800

    making sure all files are added

---
+++ assets/js/single.js
@@ -0,0 +1,50 @@
+var issueContainerEl = document.querySelector("#issues-container");
+
+const getRepoIssues = function(repo) {
+    // console.log(repo)
+    let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayIssues(data);
+            });
+        } else {
+            alert("There was a problem with your request!");
+        }
+    });
+};
+
+const displayIssues = function(issues) {
+    for (let i =0; i <issues.length; i++) {
+        // create <a> element to take users to the issue on github
+        let issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        // create span to hold issue title
+        let titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        let typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if(issues[i].pull_request) {
+            typeEl.textContent = "(Pull Request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        // append to container
+        issueEl.appendChild(typeEl);
+
+        issueContainerEl.appendChild(issueEl);
+    }
+};
+
+getRepoIssues("sydneywalcoff/code-quiz");
